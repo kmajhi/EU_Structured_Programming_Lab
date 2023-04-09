22f6b5e444b4778a2d41753e46616a6554faac5a
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-04-09 22:27:09 +0600

    Create diameter, circumference and area or circle.c

---
+++ Data Types-Lab 1/diameter, circumference and area or circle.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    float radius, diameter, circumference, area;
+
+    printf("Enter the radius of the circle: ");
+    scanf("%f", &radius);
+
+    diameter = 2 * radius;
+    circumference = 2 * 3.14 * radius;
+    area = 3.14 * radius * radius;
+
+    printf("Diameter of the circle is: %f\n", diameter);
+    printf("Circumference of the circle is: %f\n", circumference);
+    printf("Area of the circle is: %f\n", area);
+
+    return 0;
+}
