06ce669b797c025c4c37960a4c074c34ed8b454c
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-04-17 23:57:23 +0600

    Write a C program to input basic salary of an employee and calculate its Gross salary
    according to the following:
    Basic Salary <= 10000 : HRA = 20%, DA = 80%
    Basic Salary <= 20000 : HRA = 25%, DA = 90%
    Basic Salary > 20000 : HRA = 30%, DA = 95%

---
+++ Conditional Logic- Lab 2 , Lab 3/Gross salary.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+  float basicSalary, grossSalary;
+  float hra, da;
+
+  printf("Enter Basic Salary: ");
+  scanf("%f", &basicSalary);
+
+  if (basicSalary <= 10000) {
+    hra = 0.2 * basicSalary;
+    da = 0.8 * basicSalary;
+  } else if (basicSalary <= 20000) {
+    hra = 0.25 * basicSalary;
+    da = 0.9 * basicSalary;
+  } else {
+    hra = 0.3 * basicSalary;
+    da = 0.95 * basicSalary;
+  }
+
+  grossSalary = basicSalary + hra + da;
+
+  printf("Gross Salary: %.2f\n", grossSalary);
+
+  return 0;
+}
