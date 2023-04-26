2bb4dab16d7d1973d2723cb64727b2895d680467
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-04-27 00:59:18 +0600

    Write a C program to enter any number and check whether the number is palindrome or not.
    A palindrome is a word, number, phrase, or other sequences of symbols that reads the same backward as forwards, such as madam or racecar, the date and time 12/21/33 12:21, and the sentence: "A man, a plan, a canal – Panama".

---
+++ Loop, Nested Loop-Lab 4/palindrome or not check.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, reverse = 0, remainder, original;
+
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+
+    original = n;
+
+    // Reversing the number
+    while (n != 0)
+    {
+        remainder = n % 10;
+        reverse = reverse * 10 + remainder;
+        n /= 10;
+    }
+
+    // Checking if the number is a palindrome
+    if (original == reverse)
+        printf("%d is a palindrome number.", original);
+    else
+        printf("%d is not a palindrome number.", original);
+
+    return 0;
+}
