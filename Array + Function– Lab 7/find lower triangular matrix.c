7d56d952da0d121a44aa01a5eb0ae8bd5b016947
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-05-13 00:02:26 +0600

    28.	Write a C program to find lower triangular matrix.

---
+++ Array + Function– Lab 7/find lower triangular matrix.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#define ROWS 3
+#define COLS 3
+
+int main() {
+    int matrix[ROWS][COLS];
+    int i, j;
+
+    printf("Enter the elements of the matrix:\n");
+    for (i = 0; i < ROWS; i++) {
+        for (j = 0; j < COLS; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    printf("The original matrix is:\n");
+    for (i = 0; i < ROWS; i++) {
+        for (j = 0; j < COLS; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("The lower triangular matrix is:\n");
+    for (i = 0; i < ROWS; i++) {
+        for (j = 0; j < COLS; j++) {
+            if (i < j) {
+                printf("0 ");
+            } else {
+                printf("%d ", matrix[i][j]);
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
