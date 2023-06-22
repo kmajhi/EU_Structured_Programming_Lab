0b1ad034ea06c79d0873f804e40add353c1b15f5
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-06-22 21:05:04 +0600

    sum of two numbers.c

--- Data Types-Lab 1/sum of two numbers.c
+++ Data Types-Lab 1/sum of two numbers.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
 int main() {
-   int num1, num2;
+   int num1, num2, sum;
    
-   printf("Enter two integers: ");
-   scanf("%d %d", &num1, &num2);
+   printf("Enter the first integer: ");
+   scanf("%d", &num1);
+   printf("Enter the second integer: ");
+   scanf("%d", &num2);
 
-   printf("Sum of %d and %d is: %d \n", num1, num2, num1 + num2);
+   sum = num1+num2;
+
+   printf("Sum of %d and %d is: %d \n", num1, num2, sum);
    return 0;
 }
